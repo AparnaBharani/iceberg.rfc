3765a491a0190ca7ae7bb1d13d1706aaa524a218
AparnaBharani <aparnabharani2006@gmail.com>
2025-08-28 11:22:44 +0530

    Added mark task complete functionality

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ addButton.addEventListener("click", () => {
         const li = document.createElement("li");
         li.textContent = taskText;
 
+        li.style.cursor = "pointer";
+
+        li.addEventListener("click", () => {
+            li.classList.toggle("completed");
+        });
+
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "X";
         deleteBtn.style.marginLeft = "10px";
@@ -29,6 +35,6 @@ addButton.addEventListener("click", () => {
         taskList.appendChild(li);
 
         taskInput.value = "";
-        
+
     }
 });
